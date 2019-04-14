e9c2f0ac651c6b11c42b9643e40cd79f44b7daad
Carlos Fonseca <foncecacarlos@hotmail.com>
2019-04-14 18:41:22 -0400

    passando objeto state no Link

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-//import { Redirect } from 'react-router-dom'
 import Cesar from './cifraDeCesar'
 import Vegenere from './cifraDeVegenere'
 import Transposicao from './cifraDeTransposicao'
@@ -8,22 +7,46 @@ import Transposicao from './cifraDeTransposicao'
 import Header from './header'
 import Footer from './footer'
 
-export default props => (
+export default class Routes extends Component {
     
-    <Router>
-        <div>
-            <Header>
-                <Link className='link' to="/">Cifra de César</Link>
-                <Link className='link' to="/vegenere">Cifra de Vigenère</Link>
-                <Link className='link' to="/transposicao">Cifra de Transposição</Link>
-            </Header>
+    render() {
+        return(
+            <Router>
+                <div>
+                    <Header>
+                        <Link className='link' to={{
+                            pathname: "/",
+                            state: {
+                                cesar: true, 
+                                vegenere: false, 
+                                transposicao: false 
+                            }
+                            }}>Cifra de César</Link>
+                        <Link className='link'  to={{
+                            pathname: "/vegenere",
+                            state: {
+                                cesar: false, 
+                                vegenere: true, 
+                                transposicao: false 
+                            }
+                            }}>Cifra de Vigenère</Link>
+                        <Link className='link' to={{
+                            pathname: "/transposicao",
+                            state: {
+                                cesar: false, 
+                                vegenere: false, 
+                                transposicao: true 
+                            }
+                            }}>Cifra de Transposição</Link>
+                    </Header>
+                    
+                    <Route exact path='/' component={Cesar} />
+                    <Route path='/vegenere' component={Vegenere} />
+                    <Route path='/transposicao' component={Transposicao} />
 
-            <Route exact path='/' component={ Cesar } />
-            <Route path='/vegenere' component={ Vegenere } />
-            <Route path='/transposicao' component={ Transposicao } />
-            {/*<Redirect from='*' to='/' />*/}
-            
-            <Footer/>
-        </div>
-    </Router>
-)
+                    <Footer />
+                </div>
+            </Router>
+        )
+    }
+}
